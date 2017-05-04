ca5e00417cbc55672c116daa59ed9bba84fac5bc
kortv <2mothy@gmail.com>
2017-05-05 00:24:34 +0300

    remove extra reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,12 +2,10 @@ import { combineReducers } from 'redux'
 
 // Reducers
 import pokemonsReducer from './pokemonsReducer'
-// import detailsReducer from './detailsReducer'
 
 // Combine Reducers
 const reducers = combineReducers({
   pokemons: pokemonsReducer
-  // details: detailsReducer
 })
 
 export default reducers
